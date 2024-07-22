c01653943099f281ff62cdaf60e251cd3aaa3284
sandeepsaladi45 <sandeepsaladi07102004@gmail.com>
2024-07-22 14:15:20 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo;
+
+/**
+ *
+ * @author 91850
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
